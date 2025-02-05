65d4552a7d9e94e66064b46855a67ce4c0fdcb6a
angelajoshi <devanshudandekar5@gmail.com>
2025-02-06 01:13:28 +0530

    develop16

--- lib/env.dart
+++ lib/env.dart
@@ -1,9 +1,10 @@
 import 'package:envied/envied.dart';
 
-part 'env.g.dart';
+part 'env.g.dart';  // This line is crucial for code generation
 
-@Envied(path: '.env')
+@Envied(path: '.env')  // Specifies the location of your .env file
 abstract class Env {
-  @EnviedField(varName: 'SERVER_URL', obfuscate: true)
-  static String serverUrl = _Env.serverUrl;
+  @EnviedField(varName: 'SERVER_URL', obfuscate: true)  // Obfuscates the value of the environment variable
+  static String serverUrl = _Env.serverUrl;  // The generated code will provide access to this variable
 }
+
